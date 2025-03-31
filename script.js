e63453b39ad0e7695d5f181668d53e1edecf0938
Sidar <83185855+sidar-yesilyurt@users.noreply.github.com>
2025-03-31 11:32:05 +0000

    update

--- script.js
+++ script.js
@@ -266,3 +266,18 @@ document.addEventListener('DOMContentLoaded', () => {
     createReviewCarousel();
     createSeasonalSales();
 });
+
+$(document).ready(function(){
+    $('#menu').slicknav();
+
+    // Smooth scroll for in-page links
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 500);
+        }
+    });
+});
